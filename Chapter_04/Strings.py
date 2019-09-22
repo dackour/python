a171c4e78f3c81e1abe91cad7e072ce9915db60e
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-22 13:22:39 +0200

    Chapter 4 Python object types

--- Chapter_04/Strings.py
+++ Chapter_04/Strings.py
@@ -10,3 +10,22 @@ print(S[1])
 print(S[-1])
 # The second-to-last item from the end
 print(S[-2])
+# The last item in S
+print(S[-1])
+# Negative indexing, the hard way
+print(S[len(S)-1])
+# A 4-character string
+print(S)
+# Slice of S from offsets 1 through 2 (not 3)
+print(S[1:3])
+print(S[0:3])
+# Everything past the first (1:len(S))
+print(S[1:])
+# Everything but the last
+print(S[0:3])
+# Same as S[0:3]
+print(S[:3])
+# Everything but the last again, but simpler (0:-1)
+print(S[:-1])
+# All of S as top-level copy  (0:len(S))
+print(S[:])
