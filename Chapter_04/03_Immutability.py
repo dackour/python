f8b596defe7aa2d04ca2489a5ca4ba665f33301f
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-22 15:47:34 +0200

    Chapter 4 Python object types

--- Chapter_04/03_Immutability.py
+++ Chapter_04/03_Immutability.py
@@ -1,1 +1,20 @@
+S = 'Spam'
+# Immutable objects cannot be changed
+# S[0] = 'z'
+#
+# But we can run expressions to make new objects
+S = 'z' + S[1:]
+print(S)
 
+S = 'shrubbery'
+print(S)
+L = list(S)                 # Expand to a list [...]
+print(L)
+L[1] = 'c'                  # change it in place
+print(L)
+print(''.join(L))           # join with empty delimiter
+
+B = bytearray(b'spam')      # A byte/list hybrid (ahead)
+B.extend(b'eggs')           # 'b' needed in 3.X not 2.X
+print(B)                    # B[i] = ord(c) works here too
+print(B.decode())           # Translate to normal string
