0f0fb6194c32b240804f97b5f8d4b12a3e89dd71
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-22 12:58:09 +0200

    Chapter 4 Python object types

--- Chapter_04/Strings.py
+++ Chapter_04/Strings.py
@@ -1,4 +1,8 @@
+# Make a 4 character string, and assign it to a name
 S = 'Spam'
+# Lenght
 print(len(S))
+# The first item in S, indexing by zero-based position
 print(S[0])
+# The second item from the left
 print(S[1])
