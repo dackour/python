3797921f193fba027e62c2d23af4430899133cb0
Bartosz <bartosz.smela@gmail.com>
2019-09-22 12:47:31 +0200

    Chapter 4 Python object types

--- Chapter_04/numbers.py
+++ Chapter_04/numbers.py
@@ -1,6 +1,16 @@
+import math
+import random
 # Integer addition
 print(123 + 222)
 # Floating-point multiplication
 print(1.5 * 4)
 # 2 to the power 100
 print(2 ** 100)
+# How many digits in a really BIG number?
+print(len(str(2 ** 1000000)))
+# repr. as code (Pythons < 2.7 and 3.1) user friendly
+print(3.1415 * 2)
+print(math.pi)
+print(math.sqrt(85))
+print(random.random())
+print(random.choice([1, 2, 3, 4]))
