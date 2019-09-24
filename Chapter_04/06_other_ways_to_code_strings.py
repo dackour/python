0bc6c7c3a8315176cec70f78ecf02cb0099eb702
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-24 20:45:53 +0200

    Chapter 3

---
+++ Chapter_04/06_other_ways_to_code_strings.py
@@ -0,0 +1,13 @@
+S = 'A\nB\tC'       # \n is the end of line, \t is a tab
+print(S)
+print(len(S))       # each stands for just one character
+
+print(ord('\n'))    # \n is a byte with the binary value of 10 in ASCII
+
+S = 'A\0B\0C'       # \0 a binary zero byte, does not terminate string
+print(S)
+print(len(S))
+
+S = 'A\x00B\x00C'   #Non-printables are displayed as \xNN hex escapes
+print(S)
+
