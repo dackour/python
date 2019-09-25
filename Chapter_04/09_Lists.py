33201e6130b27df10846437c236298948d04c7c4
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-25 19:11:57 +0200

    Chapter 4 Python object types

--- Chapter_04/09_Lists.py
+++ Chapter_04/09_Lists.py
@@ -27,3 +27,37 @@ M.sort()
 print(M)
 M.reverse()
 print(M)
+
+print('\nNesting\n')
+
+M = [[1, 2, 3],             # A 3x3 matrix as nested list
+     [4, 5, 6],             # Code can span lines if bracketed
+     [7, 8, 9]]
+
+print(M)
+
+print(M[1])                 # Get row 2
+print(M[1][2])              # Get row 2, then get item 3
+
+print('\nComprehensions\n')
+
+col1 = [row[0] for row in M]    # Collect the items in column 1
+col2 = [row[1] for row in M]    # Give me row[1] for each row in matrix M
+col3 = [r[2] for r in M]
+print(col1)
+print(col2)
+print(col3)
+
+newColumn = [row[1] + 1 for row in M]   # Add 1 to each item in column 2
+print(newColumn)
+
+myFilter = [row[1] for row in M if row[1] % 2 == 0]   # filter out odd items
+print(myFilter)
+
+myDiag1 = [M[i][i] for i in [0, 1, 2]]  # Collect diagonal from matrix
+print(myDiag1)
+
+myDiag2 = [M[i][j] for i,j in [(0, 2), (1, 1), (2, 0)]]  # Collect reverse diagonal using tuple
+print(myDiag2)
+
+
