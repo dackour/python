61c6bf2c11c417f584e6661b22eebc2aad7d9b08
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-26 17:36:27 +0200

    Chapter 4 Python object types

--- Chapter_04/10_Dictionaries.py
+++ Chapter_04/10_Dictionaries.py
@@ -11,7 +11,7 @@ D2['age'] = 40
 print(D2)
 print(D2['name'])
 
-bob1 = dict(name= 'Bob', job= 'dev', age=40)  # Keywords
+bob1 = dict(name='Bob', job='dev', age=40)  # Keywords
 print(bob1)
 
 bob2 = dict(zip(['name', 'job', 'age'], ['Bob', 'dev', 40]))  # Zipping
@@ -32,3 +32,25 @@ print(rec['jobs'])
 
 rec = 0
 
+D3 = {'a': 1, 'b': 2, 'c': 3}
+print(D3)
+
+D3['e'] = 99  # Assigning new keys grows dictionaries
+print(D3)
+
+# D['f'] Referencing nonexistent key is an error
+
+print('f' in D3)
+
+if not 'f' in D3:  # Python sole selection statement
+    print('missing')
+
+if not 'f' in D3:
+    print('missing')
+    print('no, really')  # Statement blocks are indented
+
+value = D3.get('x', 0)  # Index but with a default
+print(value)
+
+value = D3['x'] if 'x' in D3 else 0  # if/else expression form
+print(value)
