08072e2a0ef1a3a86387101879a0ac0a02a74fbf
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 22:49:03 +0200

    Chapter 5 Numeric Types

--- Chapter_05/01_Numeric_Types.py
+++ Chapter_05/01_Numeric_Types.py
@@ -1,3 +1,11 @@
 print(40 + 3.14)  # Integer to float, float math result
 print(int(3.1415))  # Truncates float to integer
 print(float(3))  # Converts integer to float
+
+a = 3  # Name created not declared ahead of time
+b = 4
+
+print(a + 1, a -1)  # Addition 3 + 1, subtraction 3 -1
+print(b * 3, b / 2)  # Multiplication (4 * 3), division 4 / 2
+print(a % 2, b ** 2)  # Modulus (remainder), power 4 ** 2
+print(2 + 4.0, 2.0 ** b)  # Mixed type conversions
