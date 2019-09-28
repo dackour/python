b5bbd237b5a8f68ed934c6db724fdb4c5096457d
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 16:40:39 +0200

    Chapter 4 Python object types

--- Chapter_04/15_Unicode_Text_Files.py
+++ Chapter_04/15_Unicode_Text_Files.py
@@ -10,5 +10,7 @@ text = open('unidata.txt', encoding='utf8').read()  # Read/decode UTF-8 text
 print(text)
 print(len(text))  # 4 characters (code points)
 
-raw = open('unidata.txt', 'rb').read()
+raw = open('unidata.txt', 'rb').read()  # Read raw encoded bytes
 print(raw)
+print(len(raw))  # Really 5 bytes in UTF-8
+print(text.encode('utf-8'))
