42d8700c9041eb8a169de2a52a2aba2414cb3e56
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 17:28:08 +0200

    Chapter 4 Python object types

---
+++ Chapter_04/16_Other_Core_Types.py
@@ -0,0 +1,17 @@
+# Sets
+X = set('spam')  # Make a set out of a sequence in 2.x and 3.x
+print(X)
+Y = {'h', 'a', 'm'}  # Make a set with set literals in 3.x and 2.7
+print(Y)
+print(X, Y)  # A tuple of two sets without parentheses
+print(X & Y)  # Intersection
+print(X | Y)  # Union
+print(X - Y)  # Difference
+print(X > Y)  # Superset
+
+setcomp = {n ** 2 for n in [1,2,3,4]}  # Set comprehensions in 3.x and 2.7
+print(setcomp)
+
+print(list(set([1, 2, 1, 3, 1])))  # Filtering out duplicates (possibly reordered)
+print(set('spam') - set('ham'))  # Finding differences in collections
+print(set('spam') == set('asmp'))  # Order-neutral equality tests (== is False)
