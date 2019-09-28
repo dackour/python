9ecf68103111a5adb871ba3650364b960925118d
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 16:15:28 +0200

    Chapter 4 Python object types

--- Chapter_04/14_Binary_Bytes_Files.py
+++ Chapter_04/14_Binary_Bytes_Files.py
@@ -5,3 +5,10 @@ print(packed)  # 10 bytes not objects or text
 file = open('data.bin', 'wb')  # Open binary output file
 file.write(packed)  # Write packed binary data
 file.close()
+
+data = open('data.bin', 'rb').read()
+print(data)
+print(data[4:8])  # Slice bytes in the middle
+print(list(data))  # A sequence of 8-bit bytes
+data2 = struct.unpack('>i4sh', data)  # Unpack in too object again
+print(data2)
