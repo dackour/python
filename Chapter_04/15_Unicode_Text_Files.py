135de02a31e49663677c0e58adeb734295e79eeb
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 16:49:44 +0200

    Chapter 4 Python object types

--- Chapter_04/15_Unicode_Text_Files.py
+++ Chapter_04/15_Unicode_Text_Files.py
@@ -13,5 +13,11 @@ print(len(text))  # 4 characters (code points)
 raw = open('unidata.txt', 'rb').read()  # Read raw encoded bytes
 print(raw)
 print(len(raw))  # Really 5 bytes in UTF-8
-print(text.encode('utf-8'))
-print(raw.decode('utf-8'))
+print(text.encode('utf-8'))  # Manual encode to bytes
+print(raw.decode('utf-8'))  # Manual decode to str
+
+print(text.encode('latin-1'))  # Bytes differ in others
+print(text.encode('utf-16'))
+
+print(len(text.encode('latin-1')), len(text.encode('utf-16')))
+print(b'\xff\xfes\x00p\x00\xc4\x00m\x00'.decode('utf-16'))  # But same string decoded
