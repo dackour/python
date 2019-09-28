ab815e6a4a5380908d0064234e599477cf8c5312
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 14:25:46 +0200

    Chapter 4 Python object types

--- Chapter_04/13_Files.py
+++ Chapter_04/13_Files.py
@@ -1,4 +1,16 @@
 f = open('data.txt', 'w')  # Make a new file in output mode ('w' is write)
-f.write('Hello\n')
+f.write('Hello\n')  # Write string of characters to it
 f.write('world\n')
-f.close()
+f.close()  # Close to flush output buffers to disk
+
+f = open('data.txt')  # 'r' (read) is default processing mode
+text = f.read()  # Read entire file into string
+print(text)  # print interprets control characters
+
+print(text.split())  # File content is always a string
+
+for line in open('data.txt'): print(line)
+
+dir(f)
+
+help(f.seek)
