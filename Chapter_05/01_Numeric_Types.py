af28031c19ec4c50f207d82b2381dca97c158bdc
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 22:54:11 +0200

    Chapter 5 Numeric Types

--- Chapter_05/01_Numeric_Types.py
+++ Chapter_05/01_Numeric_Types.py
@@ -9,3 +9,6 @@ print(a + 1, a -1)  # Addition 3 + 1, subtraction 3 -1
 print(b * 3, b / 2)  # Multiplication (4 * 3), division 4 / 2
 print(a % 2, b ** 2)  # Modulus (remainder), power 4 ** 2
 print(2 + 4.0, 2.0 ** b)  # Mixed type conversions
+
+print(b / 2 + a)  # Same as ((4/2)+3)
+print(b / (2.0 + a))  # Same as (4/(2.0+3)
