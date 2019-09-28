2aa18aecc44df1ab22b0a379b16945b128f5fe35
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-28 18:20:16 +0200

    Chapter 4 Python object types

--- Chapter_04/16_Other_Core_Types.py
+++ Chapter_04/16_Other_Core_Types.py
@@ -70,6 +70,7 @@ class Worker:
     def giveRaise(self, percent):
         self.pay *= (1.0 + percent)  # Update pay in place
 
+
 bob = Worker('Bob Smith', 50000)  # Make two instances
 sue = Worker('Sue Jones', 60000)  # Each has the name and pay attrs
 
