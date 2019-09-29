29106c2a1bd5ec4baeb84e1482853783e943f162
Bartosz Smela <bartosz.smela@gmail.com>
2019-09-29 16:41:07 +0200

    Chapter 5 Numeric Types

---
+++ Chapter_05/03_Comparsions_Normal_and_Chained.py
@@ -0,0 +1,19 @@
+print(1 < 2)  # Less than
+print(2.0 >= 1)  # Greater than or equal mixed type 1 converted to 1.0
+print(2 == 2)  # Equal value
+print(2.0 != 2.0)  # Not equal value
+
+x = 2
+y = 4
+z = 6
+
+print(x < y < z)  # Chained comparisons range tests
+print(x < y and y < z)
+
+print(x < y > z)
+print(x < y and y > z)
+
+print(1 < 2 < 3.0 < 4)
+print(1 > 2 > 3.0 > 4)
+
+print(1 == 2 < 3)
