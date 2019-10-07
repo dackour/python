226f39420d1309f1d93043542698c286f4d2fb43
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-07 17:02:46 +0200

    Chapter 5 Numeric Types

--- Chapter_05/04_Division_Classic_Floor_and_True.py
+++ Chapter_05/04_Division_Classic_Floor_and_True.py
@@ -1,4 +1,4 @@
-print(10 / 4)
-print(10 / 4.0)
-print(10 // 4)
-print(10 // 4.0)
+print(10 / 4)  # Diff ers in 3.x keeps remainder
+print(10 / 4.0)  # Same in 3.x  keeps remainder
+print(10 // 4)  # Same in 3.x truncates remainder
+print(10 // 4.0)  # Same in 3.x truncates to floor
