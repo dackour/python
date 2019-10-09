18559e154103029cee2d771822dfcbb629a4a658
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-09 10:38:25 +0200

    Chapter 5 Numeric Types

--- Chapter_05/11_Fraction_Type.py
+++ Chapter_05/11_Fraction_Type.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+
 x = Fraction(1, 3)  # numerator  denominator
 y = Fraction(4, 6)  # Simplified to 2, 3 by gcd
 
@@ -26,6 +27,7 @@ print(0.1 + 0.1 + 0.1 - 0.3)  # This should be 0 close but not exact
 print(Fraction(1, 10) + Fraction(1, 10) + Fraction(1, 10) - Fraction(3, 10))
 
 from decimal import Decimal
+
 print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
 
 print(1 / 3)  # Use a ".0" in Python 2.x for true "/"
@@ -33,6 +35,7 @@ print(1 / 3)  # Use a ".0" in Python 2.x for true "/"
 print(Fraction(1, 3))  # Numeric accuracy two ways
 
 import decimal
+
 decimal.getcontext().prec = 2
 print(Decimal(1) / Decimal(3))
 
@@ -42,7 +45,32 @@ print(Fraction(6, 12))  # Automatically simplified
 
 print(Fraction(1, 3) + Fraction(6, 12))
 
-print(decimal.Decimal(str(1/3)) + decimal.Decimal(str(6/12)))
+print(decimal.Decimal(str(1 / 3)) + decimal.Decimal(str(6 / 12)))
 
 print(1000.0 / 1234567890)
 print(Fraction(100, 123456789))  # Substantially simpler!
+
+print((2.5).as_integer_ratio())  # Float object method
+
+f = 2.5
+z = Fraction(*f.as_integer_ratio())  # Convert float to fraction: two args
+print(z)  # Same as Fractions(5, 2) the * is a special syntax that expands tuple into individual args
+
+print(x)  # x from prior interaction
+print(x + z)  # 5/2 + 1/3 = 15/6 + 2/6
+
+print(float(x))  # Convert fraction to float
+print(float(z))
+
+print(float(x + z))
+print(17 / 6)
+
+print(Fraction.from_float(1.75))  # Convert float to fraction other way
+print(Fraction(*(1.75).as_integer_ratio()))
+
+print(x)
+print(x + 2)  # Fraction + int = Fraction
+print(x + 2.0) # Fraction + float = float
+print(x + (1./3))  # Fraction + float = float
+print(x + (4./3))
+print(x + Fraction(4, 3))  # Fraction + Fraction = Fraction
