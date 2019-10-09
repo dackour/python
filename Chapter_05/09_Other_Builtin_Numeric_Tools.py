f30041bdf1c4a7aab24de50d457e11c84afd1f69
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-09 08:24:57 +0200

    Chapter 5 Numeric Types

---
+++ Chapter_05/09_Other_Builtin_Numeric_Tools.py
@@ -0,0 +1,24 @@
+import math
+
+print(math.pi)  # Common constants
+print(math.e)
+
+print((2 * math.pi) / 180)
+print(2 * math.pi / 180)
+print(math.sin(2 * math.pi / 180))  # Sine, tangent, cosine
+
+print(math.sqrt(144), math.sqrt(2))  # Square root
+
+print(pow(2, 4), 2 ** 4, 2.0 ** 4.0)  # Exponentiation (power)
+
+print(abs(-42.0), sum((1, 2, 3, 4)))  # Absolute value, summation
+
+print(min(3, 1, 2, 4), max(3, 1, 2, 4))  # Minimum maximum
+
+print(math.floor(2.567), math.floor(-2.567))  # Floor next-lower integer
+
+print(math.trunc(2.567), math.trunc(-2.567))  # Truncate (drop decimal digits)
+
+print(int(2.567), int(-2.567))  # Truncate (integer conversion)
+
+print(round(2.567), round(2.467), round(2.567, 2))  # Round
