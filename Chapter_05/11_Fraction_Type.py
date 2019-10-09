b15e181a9eb10c4780e191e2ac97b7ed0481fd82
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-09 09:58:34 +0200

    Chapter 5 Numeric Types

---
+++ Chapter_05/11_Fraction_Type.py
@@ -0,0 +1,37 @@
+from fractions import Fraction
+x = Fraction(1, 3)  # numerator  denominator
+y = Fraction(4, 6)  # Simplified to 2, 3 by gcd
+
+print(x)
+print(y)
+
+print(x + y)
+print(x - y)  # Results are exact numerator, denominator
+print(x * y)
+
+print(Fraction('.25'))
+print(Fraction('1.25'))
+print(Fraction('.25') + Fraction('1.25'))
+
+a = 1 / 3.0  # Only asa accurate as floating-point hardware
+b = 4 / 6.0  # Can lose precision over many calculations
+print(a)
+print(b)
+print(a + b)
+print(a - b)
+print(a * b)
+
+print(0.1 + 0.1 + 0.1 - 0.3)  # This should be 0 close but not exact
+
+print(Fraction(1, 10) + Fraction(1, 10) + Fraction(1, 10) - Fraction(3, 10))
+
+from decimal import Decimal
+print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
+
+print(1 / 3)  # Use a ".0" in Python 2.x for true "/"
+
+print(Fraction(1, 3))  # Numeric accuracy two ways
+
+import decimal
+decimal.getcontext().prec = 2
+print(Decimal(1) / Decimal(3))
