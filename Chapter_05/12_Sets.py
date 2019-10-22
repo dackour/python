c06c9d884a91b98baaa514cc7e178d21ca49dc31
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-22 18:00:37 +0200

    Chapter 5 Numeric Types

--- Chapter_05/12_Sets.py
+++ Chapter_05/12_Sets.py
@@ -7,7 +7,9 @@ print(y)
 print(x - y)  # Difference
 print(x | y)  # Union
 print(x & y)  # Intersection
-print(x ^ y)  # Symmetric difference XOR
+print(x ^ y)  # Symmetric difference XOR wsystko poza czescia wspolna
 print(x > y, x < y)  # Superset, subset
 
 print('e' in x)  # Membership sets
+
+print('e' in 'Camelot', 22 in [11, 22, 33])  # But works on other types too
