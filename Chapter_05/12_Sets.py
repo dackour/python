42f30fdcc5aef2283361e57c09be29dae8fbd3ad
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-22 17:21:28 +0200

    Chapter 5 Numeric Types

--- Chapter_05/12_Sets.py
+++ Chapter_05/12_Sets.py
@@ -2,6 +2,10 @@ x = set('abcde')
 y = set('bdyxz')
 
 print(x)
+print(y)
 
 print(x - y)  # Difference
 print(x | y)  # Union
+print(x & y)  # Intersection
+print(x ^ y)  # Symmetric difference XOR
+print(x > y, x < y)  # Superset, subset
