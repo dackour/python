732ae5215dca5d4ec614fc74b8e5fc0a90be2490
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-28 17:06:28 +0100

    Chapter 6 The Dynamic Typing Interlude

---
+++ Chapter_06/01_Case_of_the_Missing_Declaration_Statements.py
@@ -0,0 +1,11 @@
+a = 3  # Assign a name to an object
+print(a)
+
+a = 3  # Its an Integer
+a = 'spam'  # Now its a string
+a = 1.23  # Now its a floating point
+
+x = 42
+x = 'shrubbery'  # Reclaim 42 now (unless referenced elsewhere)
+x = 3.1415  # Reclaim 'shruberry' now
+x = [1, 2, 3]  # Reclaim 3.1415 now
