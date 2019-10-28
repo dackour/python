60878d5a3dd4b6ef29f1ac8f7e2882b04426ac5a
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-28 18:02:46 +0100

    Chapter 6 The Dynamic Typing Interlude

--- Chapter_06/02_Shared_References.py
+++ Chapter_06/02_Shared_References.py
@@ -24,3 +24,21 @@ L2 = L1[:]  # Make a copy of L1 (or list(L1), copy.copy(L1), etc.# )
 L1[0] = 24
 print(L1)
 print(L2)  # L2 is not changed
+
+import copy
+Y = [0, 1, 2]
+X = copy.copy(Y)  # Make top-level "shallow" copy of any object Y
+X = copy.deepcopy(Y)  # Make deep copy of an object Y: copy all nested parts
+
+x = 42
+x = 'shrubbery'  # Reclaim 42 now?
+
+L = [1, 2, 3]
+M = L  # L and M reference the same object
+print(L == M)  # Same values
+print(L is M)  # Same objects
+
+L = [1, 2, 3]
+M = [1, 2, 3]  # L and M reference different objects
+print(L == M)  # Same values
+print(L is M)  # Different objects
