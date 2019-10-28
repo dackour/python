6e6bdf01583d541a5838287a513a547df012889a
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-28 16:20:55 +0100

    Chapter 5 Numeric Types

---
+++ Chapter_05/13_Booleans.py
@@ -0,0 +1,6 @@
+print(type(True))
+print(isinstance(True, int))
+print(True == 1)  # Same value
+print(True is 1)  # But a different object: see the next chapter
+print(True or False)  # Same as 1 or 0
+print(True + 4)  # Hmm wtf?
