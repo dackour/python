0904d66883bc6b7bfccb45711dac8df24b54d38f
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-30 20:55:18 +0100

    Chapter 7 String Fundamentals

--- Chapter_07/02_Strings_in_Action.py
+++ Chapter_07/02_Strings_in_Action.py
@@ -15,3 +15,16 @@ print('spam' in 'abcspamdef')  # Substring search, no position returned
 S = 'spam'
 print(S[0], S[-2])  # Indexing from front or end -2 (4+(-2))
 print(S[1:3], S[1:], S[:-1])  # Slicing extract section
+
+S = 'abcdefghijklmnop'
+print(S[1:10:2])  # Skipping items
+print(S[::2])
+print('hello'[::-1])  # Reversing items
+
+S = 'abcedfg'
+print(S[5:1:-1])  # bounds roles differ
+
+print('spam'[1:3])  # Slicing syntax
+print('spam'[slice(1, 3)])  # Slice objects with index syntax + object
+print('spam'[::-1])
+print('spam'[slice(None, None, -1)])
