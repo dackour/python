4a6ae8542a5e8a11c0e69db94da492a1da687108
Bartosz Smela <bartosz.smela@gmail.com>
2019-10-30 20:14:53 +0100

    Chapter 7 String Fundamentals

--- Chapter_07/02_Strings_in_Action.py
+++ Chapter_07/02_Strings_in_Action.py
@@ -1,3 +1,6 @@
 print(len('abc'))  # Length number of items
 print('abc' + 'def')  # Concatenation a new string
 print('Ni!' * 7)  # Repetition like 'Ni!' + 'Ni!' + ...
+
+print('------...more...---')  # 80 dashes the hard way
+print('-' * 80)  # 80 dashes the easy way
