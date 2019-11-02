2f138c662deda627e768da978295173ec2893523
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-02 15:42:10 +0100

    Chapter 7 String Fundamentals

--- Chapter_07/02_Strings_in_Action.py
+++ Chapter_07/02_Strings_in_Action.py
@@ -43,3 +43,23 @@ print(float(text))  # Shows more digits before 2.7 and 3.1
 
 print(ord('s'))
 print(chr(115))
+
+S = '5'
+S = chr(ord(S) + 1)
+print(S)
+S = chr(ord(S) + 1)
+print(S)
+
+print(int('5'))
+print(ord('5') - ord('0'))
+
+B = '1101'  # Convert binary digits to integer with ord
+I = 0
+while B != '':
+    I = I * 2 + (ord(B[0]) - ord('0'))
+    B = B[1:]
+
+print(I)
+
+print(int('1101', 2))  # Convert binary to integer built-int
+print(bin(13))  # Convert integer to binary: built-in
