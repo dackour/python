64a4cf17c788f63248e90476a355ebf37ff5cb68
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-04 17:05:03 +0100

    Chapter 7 String Fundamentals

--- Chapter_07/02_Strings_in_Action.py
+++ Chapter_07/02_Strings_in_Action.py
@@ -72,3 +72,10 @@ print(S)
 S = S[:4] + 'Burger' + S[-1]
 print(S)
 
+S = 'splot'
+S = S.replace('pl', 'pamal')
+print(S)
+
+print('That is %d %s bird!' % (1, 'dead'))  # Format expression: all Pythons
+print('That is {0} {1} bird!'.format(1, 'dead'))  # Format method in 2.6, 2.7, 3.x
+
