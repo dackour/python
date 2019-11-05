c120204cdd9331975d74f917a79d7e48d47531a8
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-05 16:50:32 +0100

    Chapter 7 String Fundamentals

--- Chapter_07/05_String_Formatting_Method_Calls.py
+++ Chapter_07/05_String_Formatting_Method_Calls.py
@@ -1,2 +1,11 @@
 template = '{0}, {1} and {2}'  # By position
 print(template.format('spam', 'ham', 'eggs'))
+
+template = '{motto}, {pork} and {food}'  # By keyword
+print(template.format(motto='spam', pork='ham', food='eggs'))
+
+template = '{motto}, {0} and {food}'  # By both
+print(template.format('ham', motto='spam', food='eggs'))
+
+template = '{}, {} and {}'  # By relative position
+print(template.format('spam', 'ham', 'eggs'))  # New in 3.1 and 2.7
