cb3d0c5fab6e40cac4a5011e713ec185d4964fb0
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-05 20:03:57 +0100

    Chapter 7 String Fundamentals

--- Chapter_07/05_String_Formatting_Method_Calls.py
+++ Chapter_07/05_String_Formatting_Method_Calls.py
@@ -146,3 +146,16 @@ print('%s' % money(296999.2567))
 print('%s %s' % tuple(commas(x) for x in (9999999, 8888888)))
 print(''.join(commas(x) for x in (9999999, 8888888)))
 '''
+
+print('{0:b}'.format((2 ** 16) - 1))  # Expression (only) binary format code
+#print('%b' % ((2 ** 16) - 1))
+print(bin((2 ** 16) - 1))  # But other more general options work too
+print('%s' % bin((2 ** 16) - 1))  # Usable with both method and % expression
+print('{}'.format(bin((2 ** 16) - 1)))  # With 2.7/3.1+ relative numbering
+print('%s' % bin((2 ** 16) - 1)[2:])  # Slice off 0b to get exact equivalent
+
+print('{:,d}'.format(999999999999))  # New str.format method feature in 3.1/2.7
+'''
+# Not working because of module formats
+print('%s' % commas(999999999999)  # But % is same with simple 8-line function
+'''
