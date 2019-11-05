fb4b1a6a4e3acde3bfb79039404486f528fc8fc4
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-05 18:14:13 +0100

    Chapter 7 String Fundamentals

--- Chapter_07/05_String_Formatting_Method_Calls.py
+++ Chapter_07/05_String_Formatting_Method_Calls.py
@@ -54,3 +54,22 @@ print('{:>10} = {:<10}'.format('spam', 123.4567))
 
 print('{0.platform:>10} = {1[kind]:<10}'.format(sys, dict(kind='laptop')))
 print('{.platform:>10} = {[kind]:<10}'.format(sys, dict(kind='laptop')))
+
+print('{0:e}, {1:.3e}, {2:g}'.format(3.14159, 3.14159, 3.14159))
+
+print('{0:f}, {1:.2f}, {2:06.2f}'.format(3.14159, 3.14159, 3.14159))
+
+print('{0:X}, {1:o}, {2:b}'.format(255, 255, 255))  # Hex, octal, binary
+
+print(bin(255), int('11111111', 2), 0b11111111)  # Other to/from binary
+
+print(hex(255), int('FF', 16), 0xFF)  # Other to/from hex
+
+print(oct(255), int('377', 8), 0o377)  # Other to/from octal, in 3.X
+                                       # 2.X prints and accepts 0377
+
+print('{0:.2f}'.format(1 / 3.0))  # Parameters hardcoded
+print('%.2f' % (1 / 3.0))  # Ditto for expression
+
+print('{0:.{1}f}'.format(1 / 3.0, 4))  # Take value from arguments
+print('%.*f' % (4, 1 / 3.0))  # Ditto for expression
