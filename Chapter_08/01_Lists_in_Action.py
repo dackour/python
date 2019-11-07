cf8d3b02c91cc58d0b1fb52300c6970771901dd4
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-07 19:14:53 +0100

    Chapter 8 Lists and Dictionaries

--- Chapter_08/01_Lists_in_Action.py
+++ Chapter_08/01_Lists_in_Action.py
@@ -1,4 +1,30 @@
 print(len([1, 2, 3]))  # Length
 
-print([1, 2, 3] + [1, 2, 3])  # Concatenation
+print([1, 2, 3] + [4, 5, 6])  # Concatenation
 
+print(['Ni!'] * 4)  # Repetition
+
+print(str([1, 2]) + '34')  # Same as "[1, 2]" + "34"
+
+print([1, 2] + list('34'))  # Same as [1, 2] + ['3', '4']
+
+print(3 in [1, 2, 3])  # Membership
+
+for x in [1, 2, 3]:
+    print(x, end=' ')  # Iteration (2.X uses: print x,)
+
+print('\n')
+
+res = [c * 4 for c in 'SPAM']  # List comprehensions
+print(res)
+
+res = []
+for c in 'SPAM':  # List comprehension equivalent
+    res.append(c * 4)
+print(res)
+
+print(list(map(abs, [-1, -2, 0, 1, 2])))
+
+L = ['spam', 'Spam', 'SPAM!']
+print(L[2])  # Offset start at zero
+print(L[-2])  # Negative count from the right
