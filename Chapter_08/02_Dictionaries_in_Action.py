a80c3f53c366be2b1b826981d1094af4b75b8a10
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-08 18:08:49 +0100

    Chapter 8 Lists and Dictionaries

--- Chapter_08/02_Dictionaries_in_Action.py
+++ Chapter_08/02_Dictionaries_in_Action.py
@@ -9,3 +9,34 @@ print(D.keys())
 
 print(D)
 D['ham'] = ['grill', 'bake', 'fry']  # Change entry (value = list)
+print(D)
+del D['eggs']  # Delete entry
+print(D)
+D['brunch'] = 'bacon'  # Add new entry
+print(D)
+
+D = {'spam': 2, 'ham': 1, 'eggs': 3}
+print(list(D.values()))
+print(list(D.items()))
+
+print(D.get('spam'))
+print(D.get('toast'))  # A key that is missing
+print(D.get('toast', 88))
+
+print(D)
+D2 = {'toast': 4, 'muffin': 5}  # Lots of delicious scrambled order here
+D.update(D2)
+print(D)
+
+# pop a dictionary by key
+print(D)
+print(D.pop('muffin'))
+print(D.pop('toast'))  # Delete and return from a key
+print(D)
+
+# pop a list by position
+L = ['aa', 'bb', 'cc', 'dd']
+print(L.pop())  # Delete and return from the end
+print(L)
+print(L.pop(1))  # Delete from a specific position
+print(L)
