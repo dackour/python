0426a1cb629576a9f488c7c70720d3845c4344b7
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-13 22:02:20 +0100

    Chapter 9 Tuples, Files, and Everything Else

--- Chapter_09/02_Files.py
+++ Chapter_09/02_Files.py
@@ -1,2 +1,4 @@
 myfile = open('myfile.txt', 'w')  # Open for text output: create/empty
-myfile.write('hello text file\n')
+myfile.write('hello text file\n')  # Write a line of text: string
+myfile.write('goodbye text file\n')
+myfile.close()
