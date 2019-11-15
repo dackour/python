434d379de2531f495dc7028260aad219360ba8e9
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-15 18:02:45 +0100

    Chapter 9 Tuples, Files, and Everything Else

--- Chapter_09/02_Files.py
+++ Chapter_09/02_Files.py
@@ -75,3 +75,21 @@ print(E)
 
 print(open('datafile.pkl', 'rb').read())  # Format is prone to change!
 #help(pickle)
+
+name = dict(first='Bob', last='Smith')
+rec = dict(name=name, job=['dev', 'mgr'], age=40.5)
+print(rec)
+
+import json
+print(json.dumps(rec))
+
+S = json.dumps(rec)
+print(S)
+
+O = json.loads(S)
+print(O)
+
+json.dump(rec, fp=open('testjson.txt', 'w'), indent=4)
+print(open('testjson.txt').read())
+P = json.load(open('testjson.txt'))
+print(P)
