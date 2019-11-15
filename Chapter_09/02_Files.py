2a2dcb0de220f1b924e85ba0e577f89f24910bb3
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-15 18:34:48 +0100

    Chapter 9 Tuples, Files, and Everything Else

--- Chapter_09/02_Files.py
+++ Chapter_09/02_Files.py
@@ -15,16 +15,15 @@ for line in open('myfile.txt'):  # Use file iterators, not reads
     print(line, end='')
 
 
-
-#mybinfile = open('data.bin', 'wb')
-#bytearray(newFileBytes)
-#newFileBytes = bytearray(b'\x00\x00\x00\x07spam\x00\x08')
-#bytes(newFileBytes)
-#mybinfile.write(bytes(newFileBytes))
-#data = open('data.bin', 'rb').read()  # open binary file rb = read binary
-#print(data)  # bytes sting holds binary data
-#print(data[4:8])  # Act like strings
-#print(data[4:8][0])  # But really are small 8-bit integers
+import struct
+mybinfile = open('data.bin', 'wb')
+bindata = struct.pack('>i4sh', 7, b'spam', 8)
+mybinfile.write(bindata)
+data = open('data.bin', 'rb').read()  # open binary file rb = read binary
+data  # bytes sting holds binary data
+data[4:8] # Act like strings
+#data[4:8][0]  # But really are small 8-bit integers
+prin
 
 X, Y, Z = 43, 44, 45  # Native Python objects
 S = 'Spam'  # Must be string to store in file
@@ -101,3 +100,10 @@ csvfile.close()
 import csv
 rdr = csv.reader(open('csvdata.txt'))
 for row in rdr: print(row)
+
+F = open('data2.bin', 'wb')  # Open binary output file
+import struct
+data = struct.pack('>i4sh', 7, b'spam', 8)  # Make packed binary data
+print(data)
+F.write(data)  # Write byte string
+F.close()
