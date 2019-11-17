b59bc02fc21716fc96b62d373026513433b25257
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-17 13:36:36 +0100

    Chapter 10 Introducing Python Statements

--- Chapter_10/01_A_Tale_of_Two_ifs.py
+++ Chapter_10/01_A_Tale_of_Two_ifs.py
@@ -18,3 +18,24 @@ else:
 
 a = 1; b = 2; print(a + b)  # Three statements on one line
 
+mylist = [1111,
+          2222,
+          3333]
+
+A, B, C, D = 1, 2, 3, 4
+
+X = (A + B +
+     C + D)
+
+if (A == 1 and
+    B == 2 and
+    C == 3):
+    print('spam' * 3)
+
+X = A + B + \
+    C + D  # An error prone old alternative
+
+if x > y: print(x)
+
+if x > y:
+    print(x)
