56c27be76704938e4d83f5e020afd764c99bfb47
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-17 16:51:27 +0100

    Chapter 10 Introducing Python Statements

--- Chapter_10/02_Interactive_Loops.py
+++ Chapter_10/02_Interactive_Loops.py
@@ -1,5 +1,5 @@
 # Interactive Loops
-'''
+
 while True:
     reply = input('Enter text: ')
     if reply == 'stop': break
@@ -46,7 +46,7 @@ while True:
     except:
         print('Bad!' * 8)
 print('Bye')
-'''
+
 
 while True:
     reply = input('Enter text: ')
@@ -55,4 +55,19 @@ while True:
         print(float(reply) ** 2)
     except:
         print('Bad!' * 8)
+print('Bye')
+
+
+while True:
+    reply = input('Enter text: ')
+    if reply == 'stop':
+        break
+    elif not reply.isdigit():
+        print('Bad!' * 8)
+    else:
+        num = int(reply)
+        if num < 20:
+            print('low')
+        else:
+            print(num ** 2)
 print('Bye')
