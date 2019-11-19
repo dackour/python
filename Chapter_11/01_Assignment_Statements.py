58f46ca80160a422f791ced852805990ffcf1833
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-19 17:41:51 +0100

    Chapter 11 Assignments, Expressions, and Prints

--- Chapter_11/01_Assignment_Statements.py
+++ Chapter_11/01_Assignment_Statements.py
@@ -89,3 +89,66 @@ while L:
     front, *L = L  # Get first, rest without slicing
     print(front, L)
 
+seq = [1, 2, 3, 4]
+a, b, c, *d = seq
+print(a, b, c, d)
+
+a, b, c, d, *e = seq
+print(a, b, c, d, e)
+
+a, b, *e, c, d
+print(a, b, c, d, e)
+
+#a, *b, c, *d, e = seq  # Error
+#a,b = seq
+#*a = seq
+
+*a, = seq
+print(a)
+
+print(seq)
+a, *b = seq  # First, rest
+print(a, b)
+
+a, b = seq[0], seq[1:]  # First, rest: traditional
+print(a, b)
+
+*a, b = seq  # Rest last
+print(a, b)
+
+a, b = seq[:-1], seq[-1]  # Rest, last: traditional
+print(a, b)
+
+for (a, *b, c) in [(1, 2, 3, 4), (5, 6, 7, 8)]:
+    print(a, b, c)
+
+for (a, b, c) in [(1, 2, 3), (4, 5, 6)]:
+    print(a, b, c)
+
+for all in [(1, 2, 3, 4), (5, 6, 7, 8)]:
+    a, b, c = all[0], all[1:3], all[3]
+    print(a, b, c)
+
+# Multiple target assignments
+a = b = c = 'spam'
+print(a, b, c)
+
+c = 'spam'
+b = c
+a = b
+print(a, b, c)
+
+a = b = 0
+b = b + 1
+print(a, b)
+
+a = b = []
+b.append(42)
+print(a, b)
+
+a = []
+b = []  # a and b do not share the same object
+b.append(42)
+print(a, b)
+
+a, b = [], []  # a and b do not share the same  object
