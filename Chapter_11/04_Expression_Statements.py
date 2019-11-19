f9eba323c0a414fcb71c4092c11e19419ec3f094
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-19 19:33:40 +0100

    Chapter 11 Assignments, Expressions, and Prints

--- Chapter_11/04_Expression_Statements.py
+++ Chapter_11/04_Expression_Statements.py
@@ -1,1 +1,11 @@
 # Expression Statements
+x = print('spam')  # Print is a function call expression in 3.X
+print(x)  # But it is coded as an expression statement
+
+L = [1, 2]
+L.append(3)  # Append is an in-place change
+print(L)
+
+# Don't do this:
+L = L.append(4)  # But append returns None, not L
+print(L)  # So we lose our list
