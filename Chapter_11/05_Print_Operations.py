3c74070db866d7edb45013db48ee2f23a276428c
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-20 20:34:38 +0100

    Chapter 11 Assignments, Expressions, and Prints

--- Chapter_11/05_Print_Operations.py
+++ Chapter_11/05_Print_Operations.py
@@ -23,5 +23,54 @@ text = '%s: %-.4f, %05d' % ('Result', 3.14159, 42)
 print(text)
 print('%s: %-.4f, %05d' % ('Result', 3.14159, 42))
 
+print('hello world')  # Print a string object in 3.x
 
+import sys  # Printing the hard way
+sys.stdout.write('hello world\n')
 
+X, Y = ['spam', 'eggs']
+
+print(X, Y)
+sys.stdout.write(str(X) + ' ' + str(Y) + '\n')
+
+#sys.stdout = open('log.txt', 'a')  # Redirects prints to a file
+print(x, y, x)  # Shows up in log.txt
+
+temp = sys.stdout  # save for restoring later
+sys.stdout = open('log.txt', 'a')  # Redirects prints to a file
+print('spam')  # prints go to file not here
+print(1, 2, 3)
+sys.stdout.close()  # Flush output to disk
+sys.stdout = temp  # Restore original stream
+print('back here')  # Prints show up here again
+print(open('log.txt').read())  # Result of earlier prints
+
+log = open('log.txt', 'a')  # 3.x
+print(x, y, z, file=log)  # Print to file like object
+print(x, y, z)  # print to original stdout
+
+#log = open('log.txt', 'a')  # 2.x
+#print >> log, x, y, z
+#print x y z
+
+log = open('log.txt', 'a')
+print(1, 2, 3, file=log)
+print(4, 5, 6, file=log)
+log.close()
+print(7, 8, 9)
+
+sys.stderr.write(('Bad!' * 8) + '\n')
+print('Bad!' * 8, file=sys.stderr)
+print(sys.stderr)
+
+X = 1
+Y = 2
+print(X, Y)  # Print the easy way
+sys.stdout.write(str(X) + ' ' + str(Y) + '\n')  # Print the hard way
+
+print(X, Y, file=open('temp1.txt', 'w'))  # Redirect text to file
+
+open('temp2.txt', 'w').write(str(X) + ' ' + str(Y) + '\n')  # Send to a file manually
+
+print(open('temp1.txt', 'rb').read())  # Binary mode for bytes
+print(open('temp2.txt', 'rb').read())
