2deba18d19c184c0dc4d906ebbedda185e56fab8
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-20 19:53:03 +0100

    Chapter 11 Assignments, Expressions, and Prints

--- Chapter_11/05_Print_Operations.py
+++ Chapter_11/05_Print_Operations.py
@@ -7,3 +7,21 @@ print(x, y, z)  # print 3 objects per defaults
 
 print(x, y, z, sep='')  # Suppress separator
 print(x, y, z, sep=', ')  # Custom separator
+
+print(x, y, z, end='')  # Suppress line break
+print(x, y, z, end=''); print(x, y, z)  # 2 prints same output line
+print(x, y, z, end='...\n')  # Custom line end
+
+print(x, y, z, sep='...', end='!\n')  # Multiple keywords
+print(x, y, z, end='!\n', sep='...')  # Order doesnt matter
+
+print(x, y, z, sep='...', file=open('data.txt', 'w'))  # Print to a file
+print(x, y, z)  # Back to stdout
+print(open('data.txt').read())  # Display file text
+
+text = '%s: %-.4f, %05d' % ('Result', 3.14159, 42)
+print(text)
+print('%s: %-.4f, %05d' % ('Result', 3.14159, 42))
+
+
+
