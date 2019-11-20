f7c56670d3189ed32706b79ad49847445f175d20
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-20 19:26:58 +0100

    Chapter 11 Assignments, Expressions, and Prints

--- Chapter_11/05_Print_Operations.py
+++ Chapter_11/05_Print_Operations.py
@@ -1,1 +1,9 @@
 # Print operations
+print()  # Display a blank line
+x = 'spam'
+y = 99
+z = ['eggs']
+print(x, y, z)  # print 3 objects per defaults
+
+print(x, y, z, sep='')  # Suppress separator
+print(x, y, z, sep=', ')  # Custom separator
