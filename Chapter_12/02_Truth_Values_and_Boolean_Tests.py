df7776089f14cf590cf42b8ef4ceda4271883497
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-22 19:33:46 +0100

    Chapter 12 If Tests and Syntax Rules

---
+++ Chapter_12/02_Truth_Values_and_Boolean_Tests.py
@@ -0,0 +1,13 @@
+# Truth_Values_and_Boolean_Tests
+X = 0
+Y = 0
+X and Y  # Is true if both are true
+X or Y  # Is true if either is true
+not X  # Is true if X is false
+
+print(2 < 3, 3 < 2)  # Less than return True or False (1 or 0)
+
+print(2 or 3, 3 or 2)  # Return left operand if true
+# Else return right operand (true or false)
+
+print()
