e39877ccef72a98a9771da12fef52aaf53d43c6b
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-24 13:41:04 +0100

    Chapter 13 If While and for Loops

--- Chapter_13/02_For_Loops.py
+++ Chapter_13/02_For_Loops.py
@@ -1,1 +1,67 @@
 # For Loops
+
+for x in ['spam', 'eggs', 'ham']:
+    print(x, end=' ')
+
+print('\n')
+
+sum = 0
+for x in [1, 2, 3, 4]:
+    sum = sum + x
+
+print(1 + 2 + 3 + 4)
+print(sum)
+
+prod = 1
+for item in [1, 2, 3, 4]: prod *= item
+print(prod)
+print(1 * 2 * 3 * 4)
+
+S = 'lumberjack'
+T = ("and", "I'm", "okay")
+
+for x in S: print(x, end=' ')  # Iterate over a string
+
+print('\n')
+
+for x in T: print(x, end=' ')  # Iterate over a tuple
+
+print('\n')
+
+T = [(1, 2), (3, 4), (5, 6)]
+for (a, b) in T:  # Tuple assignment at work
+    print(a, b)
+
+D = {'a': 1, 'b': 2, 'c': 3}
+for key in D:
+    print(key, '=>', D[key])  # Use dict keys iterator and index
+
+print(list(D.items()))
+for (key, value) in D.items():  # Iterate over both keys and values
+    print(key, '=>', value)
+
+print(T)
+for both in T:
+    a, b = both  # Manual assignment equivalent
+    print(a, b)
+
+((a, b), c) = ((1, 2), 3)  # Nested sequences work too
+print(a, b, c)
+for ((a, b), c) in [((1, 2), 3), ((4, 5), 6)]: print(a, b, c)
+for ((a, b), c) in [((1, 2), 3), ['XY', 6]]: print(a, b, c)
+
+a, b, c = (1, 2, 3)  # Tuple assignment
+print(a, b, c)
+for (a, b, c) in [(1, 2, 3), (4, 5, 6)]:  # Used in for loop
+    print(a, b, c)
+
+a, *b, c = (1, 2, 3, 4)  # Extended seq assignment
+print(a, b, c)
+for (a, *b, c) in [(1, 2, 3, 4), (5, 6, 7, 8)]:
+    print(a, b, c)
+
+for all in [(1, 2, 3, 4), (5, 6, 7, 8)]:  # Manual slicing in 2.x
+    a, b, c = all[0], all[1:3], all[3]
+    print(a, b, c)
+
+
