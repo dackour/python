f2a2ae24664af64b65b4a42205482ac66d36fc62
Bartosz Smela <bartosz.smela@gmail.com>
2019-11-25 13:33:33 +0100

    Chapter 14 Iterations and Comprehensions

--- Chapter_14/01_Iterations_first_look.py
+++ Chapter_14/01_Iterations_first_look.py
@@ -1,1 +1,7 @@
 # Iterations first look
+for x in [1, 2, 3, 4]: print(x ** 2, end=' ')
+print('\n')
+for x in (1, 2, 3, 4): print(x ** 3, end=' ')
+print('\n')
+for x in 'spam': print(x * 2, end=' ')
+print('\n')
