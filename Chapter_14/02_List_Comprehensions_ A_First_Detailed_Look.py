a542d1664759d5fadf82f21fb9f90e89804c0263
Bartosz Smela <bartosz.smela@gmail.com>
2019-12-09 18:45:06 +0100

    Chapter 14 Iterations and Comprehensions

--- Chapter_14/02_List_Comprehensions_ A_First_Detailed_Look.py
+++ Chapter_14/02_List_Comprehensions_ A_First_Detailed_Look.py
@@ -1,1 +1,18 @@
 # List Comprehensions: A First Detailed Look
+L = [1, 2, 3, 4, 5]
+for i in range(len(L)):
+    L[i] += 10
+print(L)
+
+L = [x + 10 for x in L]
+print(L)
+
+# List Comprehension Basics
+
+res = []
+for x in L:
+    res.append(x + 10)
+print(res)
+
+# Using ListComprehensions on Files
+
