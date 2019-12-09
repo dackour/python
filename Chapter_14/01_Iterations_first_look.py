0b132686628641ec9fa09bd06782923f6d178e35
Bartosz Smela <bartosz.smela@gmail.com>
2019-12-09 18:02:18 +0100

    Chapter 14 Iterations and Comprehensions

--- Chapter_14/01_Iterations_first_look.py
+++ Chapter_14/01_Iterations_first_look.py
@@ -25,7 +25,62 @@ f.__next__()
 for line in open('script2.py'):  # Use file iterators to read lines
     print(line.upper(), end='')  # Calls __next__ catches StopIteration
 
+print('\n')
+
 for line in open('script2.py').readlines():
     print(line.upper(), end='')
 
+print('\n')
+
+f = open('script2.py')
+while True:
+    line = f.readline()
+    if not line: break
+    print(line.upper(), end='')
+
+print('\n')
+
+# Manual Iteration: iter and next
+
+f = open('script2.py')
+print(f.__next__(), end='')  # Call iteration method directly
+print(f.__next__(), end='')
+
+f = open('script2.py')
+print(next(f), end='')  # The next(f) built-in calls f.__next__() in 3.x
+print(next(f), end='')  # next(f) => [3.x: f.__next__()], [2.x: f.next()]
+
+L = [1, 2, 3]
+I = iter(L)  # Obtain a iterator object from iterable
+print(I.__next__())  # Call iterators next to advance to next item
+print(I.__next__())  # Or use I.next() in 2.x, next(I) in either line
+print(I.__next__())
+#print(I.__next__())
+
+f = open('script2.py')
+print(iter(f) is f)
+print(iter(f) is f.__iter__())
+print(f.__next__())
+
+L = [1, 2, 3]
+print(iter(L) is L)
+#L.__next__()  #Err
+
+I = iter(L)
+print(I.__next__())
+print(next(I))  # Same as I.__next__()
+
+L = [1, 2, 3]
+for X in L:  # Automatic iteration
+    print(X ** 2, end=' ')  # Obtains iter, calls __next__, catches exceptions
+
+print('\n')
+
+I = iter(L)  # Manual iteration: what for loops usually do
+while True:
+    try:  # try statement catches exceptions
+        X = next(I)  # Or call I.__next__() in 3.x
+    except StopIteration:
+        break
+    print(X ** 2, end=' ')
 
