9acf661a2cf6d885643ff3edcef3af87b1eb44ed
Bartosz Smela <bartosz.smela@gmail.com>
2019-12-17 17:03:18 +0100

    Chapter 15 The Documentation Interlude

--- Chapter_15/01_Python_Documentation_sources.py
+++ Chapter_15/01_Python_Documentation_sources.py
@@ -1,1 +1,51 @@
 # Python Documentation Sources
+import  sys
+print(dir(sys))
+
+print(len(dir(sys)))  # Number names in sys
+print(len([x for x in dir(sys) if not x.startswith('__')]))  # Non __X names only
+print(len([x for x in dir(sys) if not x[0] == '_']))  # Non underscore names
+
+print(dir([]))
+print(dir(''))
+
+print(len(dir([])), len([x for x in dir([]) if not x.startswith('__')]))
+print(len(dir('')), len([x for x in dir('') if not x.startswith('__')]))
+
+print([a for a in dir(list) if not a.startswith('__')])
+print([a for a in dir(dict) if not a.startswith('__')])
+
+
+def dir1(x): return print([a for a in dir(x) if not a.startswith('__')])
+
+dir1(tuple)
+
+print(dir(str) == dir(''))  # Same result, type name or literal
+print(dir(list) == dir([]))
+
+# Docstrings
+"""
+Module documentation
+Words go here
+"""
+
+
+def square(x):
+    """
+    function documentation
+    can we have yoour liver then?
+    :param x:
+    :return:
+    """
+    return x ** 2  # Square
+
+
+class Employee:
+    "class documentation"
+    pass
+
+
+print(square(4))
+print(square.__doc__)
+
+import docstrings
