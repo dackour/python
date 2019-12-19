d2f5b24e8b14c8ed0dbd33c1f40635004e03121c
Bartosz Smela <bartosz.smela@gmail.com>
2019-12-19 22:23:14 +0100

    Chapter 17 Scopes

--- Chapter_17/02_Global_Statement.py
+++ Chapter_17/02_Global_Statement.py
@@ -42,3 +42,32 @@ def setX(new):  # accessor make eternal changes explit
 #first.setX(88)  #Call the function intead of changing directly
 
 # Other Ways to Access Globals
+# thismod.py
+var = 99  # GGlobal variable == module attribute
+
+
+def local():
+    var = 0  # Change local var
+
+
+def glob1():
+    global var  # Declare global (normal)
+    var += 1  # Change globar var
+
+
+def glob2():
+    var = 0  # Change local var
+    #import thismod  # Import myself
+    #thismod.var  # change global var
+
+def glob3():
+    var = 0  # Change local var
+    import sys  # Import system table
+    #glob = sys.modules['thismod']  # Get module object (or use __name__)
+    #glob.var += 1  # Change global var
+
+
+def test():
+    print(var)
+    local(); glob1(); glob2(); glob3()
+    print(var)
