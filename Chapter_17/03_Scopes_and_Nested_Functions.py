704e35637593bc7c57382315eb8290d9f0f92cc9
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-01 18:31:06 +0100

    Chapter 17 Scopes

--- Chapter_17/03_Scopes_and_Nested_Functions.py
+++ Chapter_17/03_Scopes_and_Nested_Functions.py
@@ -47,3 +47,59 @@ def maker(N):
 
 h = maker(3)
 print(h(4))  # 4 ** 3 again
+
+# Retaining Enclosing Scope State with defaults
+
+
+def f1():
+    x = 88
+    def f2(x=x):  # Remember enclosing scope X with defaults
+        print(x)
+    f2()
+
+
+f1()  # Prints 88
+
+
+def f1():
+    x = 88  # Pass along instead of nesting
+    f2(x)  # Forward reference OK
+
+
+def f2(x):
+    print(x)  # Flat is still often better than nested
+
+f1()
+
+
+def func():
+    x = 4
+    action = (lambda n: x ** n)  # x remembered from enclosing def
+    return action
+
+
+x = func()
+print(x(2))  # prints 16, 4 ** 2
+
+
+def func():
+    x = 4
+    action = (lambda n, x=x: x ** n)  # Pass x in manually
+    return action
+
+
+def makeActions():
+    acts = []
+    for i in range(5):  # Tries to remember each i
+        acts.append(lambda x: i ** x)  # But all remember same last it
+    return acts
+
+
+acts = makeActions()
+print(acts[0])
+
+print(acts[0](2))  # All are 4 ** 2, 4=value of last i
+print(acts[1](2))  # This should be 1 ** 2 (1)
+print(acts[2](2))  # This should be 2 ** 2 (4)
+print(acts[4](2))  # Only this should be 4 ** 2 (16)
+
