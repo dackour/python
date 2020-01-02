a54e201d1c07d476eca90d3ed386f33e1183ad95
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-02 17:36:27 +0100

    Chapter 17 Scopes

--- Chapter_17/04_The_Nonlocal_Statement.py
+++ Chapter_17/04_The_Nonlocal_Statement.py
@@ -38,3 +38,42 @@ F = tester(0)
 F('spam')  # Increments state on each call
 F('ham')
 F('eggs')
+
+G = tester(42)  # Make a new tester that starts at 42
+G('spam')
+G('eggs')  # My state information updated to 43
+F('bacon')  # But F's is where it left off at 3
+# Each call has different state information
+
+# Boundary cases
+
+'''
+def tester(start):
+    def nested(label):
+        nonlocal state  # Nonlocals must already exist in enclosing def!
+        state = 0
+        print(label, state)
+    return nested
+'''
+
+def tester(start):
+    def nested(label):
+        global state  # Globals don't have to exist yet when declared
+        state = 0  # This creates the name in the module now
+        print(label, state)
+    return nested
+
+
+F = tester(0)
+F('abc')
+print(state)
+
+'''
+spam = 99
+def tester():
+    def nested():
+        nonlocal spam  # Must be in def, not the module!
+        print('Current=', spam)
+        spam += 1
+    return nested
+'''
