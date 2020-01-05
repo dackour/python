a29f3b9c9b8ba8de72a937e6493ba1f3f1c625bd
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-05 15:16:10 +0100

    Chapter 18 Arguments

--- Chapter_18/02_Special_Argument_Matching_Modes.py
+++ Chapter_18/02_Special_Argument_Matching_Modes.py
@@ -72,3 +72,38 @@ def f(a, *pargs, **kargs): print(a, pargs, kargs)
 f(1, 2, 3, x=1, y=1)
 
 # Calls Unpacking arguments
+
+
+def func(a, b, c, d): print(a, b, c, d)
+
+
+args = (1, 2)
+args += (3, 4)
+func(*args)  # Same as func(1, 2, 3, 4)
+
+args = {'a': 1, 'b': 2, 'c': 3}
+args['d'] = 4
+func(**args)
+
+func(*(1, 2), **{'d': 4, 'c': 3})  # Same as func(1,2,d=4,c=3)
+func(1, *(2, 3), **{'d': 4})  # Same as func(1,2,3,d=4)
+func(1, c=3, *(2, ), **{'d': 4})  # Same as func(1,2,c=3,d=4)
+func(1, * (2, 3), d=4)  # Same as func(1,2,3,d=4)
+func(1, *(2, ), c=3, **{'d': 4})  # Same as func(1,2,c=3,d=4)
+
+# Applying functions generically
+
+
+def tracer(func, *pargs, **kargs):  # Accept arbitrary arguments
+    print('calling:', func.__name__)
+    return func(*pargs, **kargs)  # Pass along arbitrary arguments
+
+
+def func(a,b,c,d):
+    return a + b + c + d
+
+
+print(tracer(func, 1, 2, c=3, d=4))
+
+# The defunct apply built-in (Python 2.X)
+# Python 3.X Keyword Only Arguments
