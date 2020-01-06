4164efd5161e814965351d079f29b950324fb871
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-06 21:32:24 +0100

    Chapter 18 Arguments

--- Chapter_18/02_Special_Argument_Matching_Modes.py
+++ Chapter_18/02_Special_Argument_Matching_Modes.py
@@ -107,3 +107,111 @@ print(tracer(func, 1, 2, c=3, d=4))
 
 # The defunct apply built-in (Python 2.X)
 # Python 3.X Keyword Only Arguments
+
+
+def kwonly(a, *b, c):
+    print(a, b, c)
+
+
+kwonly(1, 2, c=3)
+kwonly(a=1, c=3)
+#kwonly(1, 2, 3)  # err
+
+
+def kwonly(a, *, b, c):
+    print(a, b,c )
+
+
+kwonly(1, c=3, b=2)
+kwonly(c=3, b=2, a=1)
+#kwonly(1, 2, 3)  # err
+#kwonly(1)  # err
+
+
+def kwonly(a, *, b='spam', c='ham'):
+    print(a, b, c)
+
+
+kwonly(1)
+kwonly(1, c=3)
+kwonly(a=1)
+kwonly(c=3, b=2, a=1)
+#kwonly(1, 2)  # err
+
+
+def kwonly(a, *, b, c='spam'):
+    print(a, b, c)
+
+
+kwonly(1, b='eggs')
+#kwonly(1, c='eggs')  # err
+#kwonly(1,2)  # err
+
+
+def kwonly(a, *, b=1, c, d=2):
+    print(a, b, c, d)
+
+
+kwonly(3, c=4)
+kwonly(3, c=4, b=5)
+#kwonly(3)  # err
+#kwonly(1, 2, 3)  # err
+
+# Ordering rules
+
+
+def f(a, *b, c=6, **d): print(a, b, c, d)  # Collect args in header
+
+
+f(1, 2, 3, x=4, y=5)  # Default used
+f(1, 2, 3, x=4, y=5, c=7)  # Override default
+f(1, 2, 3, c=7, x=4, y=5)  # Anywhere in keywords
+
+
+def f(a, c=6, *b, **d): print(a, b, c, d)  # c is not keyword-only here
+
+
+f(1, 2, 3, x=4)
+
+
+def f(a, *b, c=6, **d): print(a, b, c, d)  # KW-only between * and **
+
+
+f(1, *(2, 3), **dict(x=4, y=5))  # Unpack args at call
+f(1, *(2, 3), **dict(x=4, y=5), c=7)  # Keywords before **args
+f(1, *(2, 3), c=7, **dict(x=4, y=5))  # Override default
+f(1, c=7, *(2, 3), **dict(x=4, y=5))  # After or before *
+f(1, *(2, 3), **dict(x=4, y=5, c=7))  # Keyword only in **
+
+# Why keyword only arguments
+# The min Wakeup call
+# Full credit
+
+
+def min1(*args):
+    res = args[0]
+    for arg in args[1:]:
+        if arg < res:
+            res = arg
+    return res
+
+
+def min2(first, *rest):
+    for arg in rest:
+        if arg < first:
+            first = arg
+    return  first
+
+
+def min3(*args):
+    tmp = list(args)
+    tmp.sort()
+    return tmp[0]
+
+
+print(min1(3, 4, 1, 2))
+print(min2('bb', 'aa'))
+print(min3([2,2], [1,1], [3,3]))
+
+# Bonus Points
+# The punch Line
