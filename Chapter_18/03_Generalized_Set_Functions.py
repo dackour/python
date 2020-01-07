69792f33de7eafd586ce6a789f8fa999f0d5c6ba
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-07 20:50:10 +0100

    Chapter 18 Arguments

--- Chapter_18/03_Generalized_Set_Functions.py
+++ Chapter_18/03_Generalized_Set_Functions.py
@@ -1,1 +1,34 @@
 # Generalized Set Functions
+
+
+def intersect(*args):
+    res = []
+    for x in args[0]:  # Scan first sequence
+        if x in res: continue  # Skip duplicates
+        for other in args[1:]:  # For all other args
+            if x not in other:  # item in each one?
+                break  # No break out of loop
+            else:
+                res.append(x)  # Yes: add items to end
+    return res
+
+
+print(intersect((1, 2, 2, 3), (3, 4, 5)))
+
+
+def union(*args):
+    res = []
+    for seq in args:
+        for x in seq:
+            if not x in res:
+                res.append(x)
+    return res
+
+
+print(union((1, 2, 3, 4, 5), (5, 6, 7, 8, 9)))
+
+s1, s2, s3 = 'SPAM', 'SCAM', 'SLAM'
+
+print(intersect(s1, s2), union(s1, s2))  # Two operands
+print(intersect([1, 2, 3], (1, 4)))  # Mixed types
+print(intersect(s1, s2, s3))  # Three operands
