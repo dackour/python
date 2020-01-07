0366c0d38a5a3b3e5ab24c3a27417474484ad88e
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-07 17:54:18 +0100

    Chapter 18 Arguments

--- Chapter_18/02_Special_Argument_Matching_Modes.py
+++ Chapter_18/02_Special_Argument_Matching_Modes.py
@@ -214,4 +214,49 @@ print(min2('bb', 'aa'))
 print(min3([2,2], [1,1], [3,3]))
 
 # Bonus Points
+
+
+def max1(*args):
+    res = args[0]
+    for arg in args[1:]:
+        if arg > res:
+            res = arg
+    return res
+
+
+def max2(first, *rest):
+    for arg in rest:
+        if arg > first:
+            first = arg
+    return  first
+
+
+def max3(*args):
+    tmp = list(args)
+    tmp.sort()
+    return tmp[-1]
+
+
+print(max1(3, 4, 1, 2))
+print(max2('bb', 'aa'))
+print(max3([2,2], [1,1], [3,3]))
+
+
+def minmax(test, *args):
+    res = args[0]
+    for arg in args[1:]:
+        if test(arg, res):
+            res = arg
+    return res
+
+
+def lessthan(x, y): return x < y
+
+
+def grtrthan(x, y): return x > y
+
+
+print(minmax(lessthan, 4, 2, 1, 5, 6, 3))
+print(minmax(grtrthan, 4, 2, 1, 5, 6, 3))
+
 # The punch Line
