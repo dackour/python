f194bf4fab02e1bc5b498d61af56c963bc560758
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-09 17:05:26 +0100

    Chapter 19 Advanced Function Topics

--- Chapter_19/01_Recursive_Functions.py
+++ Chapter_19/01_Recursive_Functions.py
@@ -55,4 +55,20 @@ print(mysum([1.1, 2.2, 3.3, 4.4]))
 
 # Loop Statements Versus Recursion
 
+L = [1, 2, 3, 4, 5]
+sum = 0
+while L:
+    sum += L[0]
+    L = L[1:]
+
+print(sum)
+
+L = [1, 2, 3, 4, 5]
+sum = 0
+for x in L: sum += x
+
+print(sum)
+
+# Handling Arbitrary Structures
+
 
