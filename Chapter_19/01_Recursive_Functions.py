26ae4bc7d07c5182f0d41a29203b586c699aecc3
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-09 17:49:33 +0100

    Chapter 19 Advanced Function Topics

--- Chapter_19/01_Recursive_Functions.py
+++ Chapter_19/01_Recursive_Functions.py
@@ -72,3 +72,61 @@ print(sum)
 # Handling Arbitrary Structures
 
 
+def sumtree(L):
+    tot = 0
+    for x in L:  # for each item at this level
+        if not isinstance(x, list):
+            tot += x  # Add numbers directly
+        else:
+            tot += sumtree(x)  # Recur for sublists
+    return tot
+
+
+L = [1, [2, [3, 4], 5], 6, [7, 8]]
+print(sumtree(L))
+print(sumtree([1, [2, [3, [4, [5]]]]]))  # Prints 15 (right heavy)
+print(sumtree([[[[[1], 2], 3], 4], 5]))  # Prints 15 (left heavy)
+
+# Recursion versus queues and stacks
+
+
+def sumtree(L):  # Breadth-first, explicit queue
+    tot = 0
+    items = list(L)  # Start with copy of top level
+    while items:
+        front = items.pop()  # Fetch/delete front item
+        if not isinstance(front, list):
+            tot += front  # add numbers directly
+        else:
+            items.extend(front)  # <== Append all in nested list
+    return tot
+
+
+print(sumtree(L))
+
+
+def sumtree(L):  # Depth-first, explicit stack
+    tot = 0
+    items = list(L)  # Start with copy of to level
+    while items:
+        front = items.pop(0)  # Fetch/delete front item
+        if not isinstance(front, list):
+            tot += front  # Add numbers directly
+        else:
+            items[:0]  = front  # <== Prepend all in nested list
+    return tot
+
+
+print(sumtree([1, [2, [3, [4, [5]]]]]))
+
+# Cycle, paths, and stack limits
+
+import sys
+
+print(sys.getrecursionlimit())
+# sys.setrecursionlimit(10000)  # Allow deeper nesting
+print(help(sys.setrecursionlimit))  # Read mor about it
+
+# More recursion examples
+
+
