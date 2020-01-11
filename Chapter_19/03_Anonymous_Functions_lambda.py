e284eac46eb06511143f86bf6d522d062f01eeb9
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-11 16:49:25 +0100

    Chapter 19 Advanced Function Topics

--- Chapter_19/03_Anonymous_Functions_lambda.py
+++ Chapter_19/03_Anonymous_Functions_lambda.py
@@ -1,1 +1,31 @@
 # Anonymous Functions lambda
+# Lambda Basics
+
+
+def func(x, y, z): return x + y + z
+
+
+print(func(2, 3, 4))
+
+f = lambda x, y, z: x + y + z
+
+print(f(2, 3, 4))
+
+x = (lambda a = 'fee', b = 'fie', c = 'foe': a + b + c)
+print(x('wee'))
+
+
+def knights():
+    title = 'Sir'
+    action = (lambda x: title + ' ' + x)  # title in enclosing def scope
+    return action  # Return a function object
+
+
+act = knights()
+msg = act('Robin')  # Robin passed to x
+print(msg)
+print(act)  # act a function not its result
+
+# Why use lambda?
+
+
