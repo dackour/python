8908fc544ad55b5f2f66a0de5a8c5621894c5344
Bartosz Smela <bartosz.smela@gmail.com>
2020-01-11 13:36:58 +0100

    Chapter 19 Advanced Function Topics

--- Chapter_19/02_Function_Objects_Attributes_and_Annotations.py
+++ Chapter_19/02_Function_Objects_Attributes_and_Annotations.py
@@ -72,3 +72,39 @@ print([x for x in dir(f) if not x.startswith('__')])
 
 # Function Annotations in 3.X
 
+
+def func(a, b, c):
+    return a + b + c
+
+
+print(func(1, 2, 3))
+
+
+def func(a: 'spam', b: (1, 10), c: float) -> int:
+    return a + b + c
+
+
+print(func(1, 2, 3))
+print(func.__annotations__)
+
+
+def func(a: 'spam', b, c: 99):
+    return a + b + c
+
+
+print(func(1, 2, 3))
+print(func.__annotations__)
+
+for arg in func.__annotations__:
+    print(arg, '=>', func.__annotations__[arg])
+
+
+def func(a: 'spam' = 4, b: (1, 10) = 5, c: float = 6) -> int:
+    return a + b + c
+
+
+print(func(1, 2, 3))
+print(func())  # 4 + 5 + 6 (all defaults)
+
+print(func(1, c=10))  # 1 + 5 + 10 (keywords work normally)
+print(func.__annotations__)
