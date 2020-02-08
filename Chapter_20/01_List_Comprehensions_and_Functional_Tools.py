8fa196eccadc7d0a091e35df4bc0f5259f743502
Bartosz Smela <bartosz.smela@gmail.com>
2020-02-08 11:39:19 +0100

    Chapter 20 Comprehensions and Generations

--- Chapter_20/01_List_Comprehensions_and_Functional_Tools.py
+++ Chapter_20/01_List_Comprehensions_and_Functional_Tools.py
@@ -33,3 +33,42 @@ print([x ** 2 for x in range(10) if x % 2 == 0])
 print(list(map((lambda x: x ** 2), filter((lambda x: x % 2 == 0), range(10)))))
 
 # Formal comprehension syntax
+
+res = [x + y for x in [0, 1, 2] for y in [100, 200, 300]]
+print(res)
+
+res = []
+for x in [0, 1, 2]:
+    for y in [100, 200, 300]:
+        res.append(x + y)
+
+print(res)
+
+res = []
+res = [x + y for x in 'spam' for y in 'SPAM']
+print(res)
+
+res = []
+res = [x + y for x in 'spam' if x in 'sm' for y in 'SPAM' if y in ('P', 'A')]
+print(res)
+
+res = []
+res = [x + y + z for x in 'spam' if x in 'sm'
+       for y in 'SPAM' if y in ('P', 'A')
+       for z in '123' if z > '1']
+print(res)
+
+res = []
+res = [(x, y) for x in range(5) if x % 2 == 0 for y in range(5) if y % 2 == 1]
+print(res)
+
+res = []
+for x in range(5):
+    if x % 2 == 0:
+        for y in range(5):
+            if y % 2 == 1:
+                res.append((x, y))
+
+print(res)
+
+# Example: List Comprehensions and Matrixes
