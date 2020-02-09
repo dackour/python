cc975d99e69f7b194a21aa21a5fd29d7cee87497
Bartosz Smela <bartosz.smela@gmail.com>
2020-02-09 13:41:02 +0100

    Chapter 20 Comprehensions and Generations

--- Chapter_20/02_Generator_Functions_and_Expressions.py
+++ Chapter_20/02_Generator_Functions_and_Expressions.py
@@ -1,3 +1,27 @@
 # Generator Functions and Expressions
 
 
+def gensquares(N):
+    for i in range(N):
+        yield i ** 2  # Resume here later
+
+
+for i in gensquares(5):  # Resume function
+    print(i, end=' : ')  # Print last yielded value
+
+print('\n')
+
+x = gensquares(4)
+print(x)
+
+print(next(x))  # Same as x.__next__() in 3.x
+print(next(x))  # Use x.next() or next() in 2.x
+print(next(x))
+print(next(x))
+#print(next(x))  # StopIteration exeception
+
+y = gensquares(5)  # returns a generator which is its own iterator
+print(iter(y) is y)  # iter() is not required: a no-op here
+print(next(y))  # Can run next() immediately
+
+# Why generator functions?
