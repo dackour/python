c24db1574447de8930762b211597e0e7060aa64c
Bartosz Smela <bartosz.smela@gmail.com>
2020-02-10 10:14:51 +0100

    Chapter 20 Comprehensions and Generations

--- Chapter_20/02_Generator_Functions_and_Expressions.py
+++ Chapter_20/02_Generator_Functions_and_Expressions.py
@@ -166,3 +166,21 @@ print(L)
 
 # Generator expressions versus filter
 
+line = 'aa bbb c'
+print(''.join(x for x in line.split() if len(x) > 1))  # Generator with if
+
+print(''.join(filter(lambda x : len(x) > 1, line.split())))  # Similar to filter
+
+print(''.join(x.upper() for x in line.split() if len(x) > 1))
+
+print(''.join(map(str.upper, filter(lambda x: len(x) > 1, line.split()))))
+
+print(''.join(x.upper() for x in line.split() if len(x) > 1))
+
+res = ''
+for x in line.split():  # Statement equivalent?
+    if len(x) > 1:  # This is also a join
+        res += x.upper()
+
+print(res)
+
