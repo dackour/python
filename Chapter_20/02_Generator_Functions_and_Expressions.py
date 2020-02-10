7edd7cde6b500320c7f68f444184581a113c0058
Bartosz Smela <bartosz.smela@gmail.com>
2020-02-10 10:33:19 +0100

    Chapter 20 Comprehensions and Generations

--- Chapter_20/02_Generator_Functions_and_Expressions.py
+++ Chapter_20/02_Generator_Functions_and_Expressions.py
@@ -186,4 +186,39 @@ print(res)
 
 # Generator Functions Versus Generator Expressions
 
+G = (c * 4 for c in 'SPAM')  # Generator expression
+print(list(G))  # Force generator to produce all results
+
+
+def timesfour(S):  # Generator function
+    for c in S:
+        yield c * 4
+
+G = timesfour('spam')
+print(list(G))  # Iterate automatically
+
+G = (c * 4 for c in 'SPAM')
+I = iter(G)  # Iterate manually (expression)
+print(next(I))
+print(next(I))
+
+G = timesfour('spam')
+I = iter(G)  # Iterate manually function
+print(next(I))
+print(next(I))
+
+line = 'aa bbb c'
+
+print(''.join(x.upper() for x in line.split() if len(x) > 1))  # Expression
+
+
+def gensub(line):  # Function
+    for x in line.split():
+        if len(x) > 1:
+            yield x.upper()
+
+
+print(''.join(gensub(line)))  # But why generate
+
+# Generators Are single-Iteration Objects
 
