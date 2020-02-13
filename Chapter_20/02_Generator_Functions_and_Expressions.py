5cf537e8c905540e3d946dd079c330bd1450dd47
Bartosz Smela <bartosz.smela@gmail.com>
2020-02-13 10:45:17 +0100

    Chapter 20 Comprehensions and Generations

--- Chapter_20/02_Generator_Functions_and_Expressions.py
+++ Chapter_20/02_Generator_Functions_and_Expressions.py
@@ -317,3 +317,25 @@ print(next(I))
 # Generators and function application
 
 
+def f(a, b, c): print('%s, %s, and %s' % (a, b, c))
+
+
+f(0, 1, 2)  # Normal positionals
+f(*range(3))  # Unpack range values: iterable in 3.x
+f(*(i for i in range(3)))  # Unpack generator expressions values
+
+D = dict(a='Bob', b='dev', c=40.5)
+print(D)
+f(a='Bob', b='dev', c=40.5)  # Normal keywords
+f(**D)  # Unpack dict: key = value
+f(*D)  # Unpack keys iterator
+f(*D.values())  # Unpack view iterator: iterable in 3.X
+
+for x in 'spam': print(x.upper(), end=' ')
+print('\n')
+list(print(x.upper(), end=' ') for x in 'spam')
+print('\n')
+print(*(x.upper() for x in 'spam'))
+
+# Preview: User-defined iterables in classes
+
