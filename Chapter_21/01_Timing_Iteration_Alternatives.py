ac767180c2d3e82cfeb6f6a415383036a6feef5a
Bartosz Smela <bartosz.smela@gmail.com>
2020-02-15 22:37:56 +0100

    Chapter 21 The Benchmarking Interlude

--- Chapter_21/01_Timing_Iteration_Alternatives.py
+++ Chapter_21/01_Timing_Iteration_Alternatives.py
@@ -1,1 +1,19 @@
 # Timing Module: Homegrown
+
+# import time
+#
+#
+# def timer(func, *args): # Simplistic timing function
+#     start = time.clock()
+#     for i in range(1000):
+#         func(*args)
+#     return time.clock() - start  # Total elapsed time in seconds
+#
+#
+# timer(pow, 2, 1000)  # Time to call pow(2, 1000) 1000 times
+#
+# timer(str.upper, 'spam')  # Time to call 'spam'.upper() 1000 times
+
+# Timing Script
+# Timing Module Alternatives
+
