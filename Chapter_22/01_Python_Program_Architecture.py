a5fefb76c118f3d075ddd77cfb9fac6e032501b1
Bartosz Smela <bartosz.smela@gmail.com>
2020-03-14 14:11:40 +0100

    Chapter 22 Modules: The Big Picture

--- Chapter_22/01_Python_Program_Architecture.py
+++ Chapter_22/01_Python_Program_Architecture.py
@@ -1,2 +1,17 @@
 # Imports and Attributes
 
+import b
+
+b.spam('gumby')
+
+# Standard Library Modules
+# How Imports Work
+# 1. Find It
+# 2. Compile It (Maybe)
+# 3. Run It
+
+import sys
+
+print(sys.modules.keys())
+
+# B
