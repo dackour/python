a86fcb9e6ffe809a175f1070edf57774377e20fb
Bartosz Smela <bartosz.smela@gmail.com>
2020-03-22 11:29:54 +0100

    Chapter 23 Module Coding Basics

--- Chapter_23/01_Module_Usage.py
+++ Chapter_23/01_Module_Usage.py
@@ -1,4 +1,26 @@
 # The import statement
 
 import module1  # Get module as a whole (one or more)
-module1.printer('Hello world')
+module1.printer('Hello world!')  # Qualify to get names
+
+# The from Statement
+
+from module1 import printer  # Copy out a variable (one or more)
+printer('Hello world!')  # No need to qualify name
+
+# The from * Statement
+
+from module1 import *  # Copy out _all_variables
+printer('Hello world!')
+
+# Imports Happen Only Once
+
+import simple  # First import: loads and runs file's code
+print(simple.spam)  # Assignment makes an attribute
+simple.spam = 2  # Change attribute in module
+import simple  # Just fetches already loaded module
+printer(simple.spam)  # Code wasn't rerun: attribute unchanged
+
+# import and from Are Assignments
+
+
