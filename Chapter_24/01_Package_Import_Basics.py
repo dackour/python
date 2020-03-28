4d1cc3e70eeb2022e1371da77e4bfc4a3f37974e
Bartosz Smela <bartosz.smela@gmail.com>
2020-03-28 16:39:22 +0100

    Chapter 24 Module Packages

--- Chapter_24/01_Package_Import_Basics.py
+++ Chapter_24/01_Package_Import_Basics.py
@@ -1,1 +1,7 @@
 # Package Import Basics
+# Packages and Search Path Settings
+# Package __init__.py Files
+# Package initialization file roles
+# From Versus import with Packages
+# Why Use Package Imports
+# A Tale of three systems
