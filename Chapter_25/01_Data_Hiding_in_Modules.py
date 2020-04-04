605a02cc927ae8e1915872fdb2351ea8bb7f6cbb
Bartosz Smela <bartosz.smela@gmail.com>
2020-04-04 10:15:33 +0200

    Chapter 25 Advanced Module Topics

--- Chapter_25/01_Data_Hiding_in_Modules.py
+++ Chapter_25/01_Data_Hiding_in_Modules.py
@@ -1,3 +1,23 @@
 # Advanced Module Topics
 # Module design Concepts
 # Data Hiding in Modules
+# Minimizing from * Damage: _X and __all__
+from unders import *  # Load non _X names only
+print(a, c)
+# print(_b)
+
+import unders  # But other importers get every name
+print(unders._b)
+
+from alls import *  # Load __all__names only
+print(a, _c)
+# print(b)
+
+from alls import a, b, _c, _d  # But other importers get every name
+print(a, b, _c, _d)
+
+import alls
+print(alls.a, alls.b, alls._c, alls._d)
+
+# Enabling Future Language Features: __future__
+
