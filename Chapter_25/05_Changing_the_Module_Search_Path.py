12d2d809231ecfeaa767837f1a59baa1615b9eb9
Bartosz Smela <bartosz.smela@gmail.com>
2020-04-04 15:36:05 +0200

    Chapter 25 Advanced Module Topics

--- Chapter_25/05_Changing_the_Module_Search_Path.py
+++ Chapter_25/05_Changing_the_Module_Search_Path.py
@@ -1,1 +1,14 @@
 # Changing the Module Search Path
+import sys
+print(sys.path)
+
+# sys.path.append('C:\\tmp')  # Extend module search path
+# import string
+
+# sys.path = [r'C:\tmp']  # Change module search path
+# sys.path.append('C:\\Windows')  # For this run (process) only
+# sys.path.insert(0, '..')
+# print(sys.path)
+# import string
+
+# The as Extension for import and from
