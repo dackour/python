35093e941c021c174277bb1a036e330bb964b6a5
Bartosz Smela <bartosz.smela@gmail.com>
2020-04-04 16:04:35 +0200

    Chapter 25 Advanced Module Topics

--- Chapter_25/07_Importing_Modules_by_Name_String.py
+++ Chapter_25/07_Importing_Modules_by_Name_String.py
@@ -1,1 +1,20 @@
 # Importing Modules by Name String
+# import 'string'
+# x = 'string'
+# import x
+
+# Running Code Strings
+
+modname = 'string'
+exec('import ' + modname)  # Run a string of code
+print(string)  # Imported in this namespace
+
+# Direct Calls: Two Options
+modname = 'string'
+string = __import__(modname)
+print(string)
+
+import importlib
+modname = 'string'
+string = importlib.import_module(modname)
+print(string)
