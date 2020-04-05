22d4c08cb0a322471af2ae3103f1e31fe9603f3b
Bartosz Smela <bartosz.smela@gmail.com>
2020-04-05 10:29:49 +0200

    Chapter 25 Advanced Module Topics

--- Chapter_25/09_Module_Gotchas.py
+++ Chapter_25/09_Module_Gotchas.py
@@ -1,1 +1,19 @@
 # Module Gotchas
+# Module Name Clashes: Package and Package-Relative Imports
+# Statement Order Matters in Top-Level Code
+
+# func1()  # Error: "func1" not yet assigned
+
+
+def func1():
+    print(func2())  # OK "func2" looked up later
+
+
+# func1()  # Error: "func2" not yet assigned
+
+
+def func2():
+    return 'Hello'
+
+
+func1()  # OK: func1() and func2() assigned
